e3a3bea8fca0a225c378c8a6ec11da105e48a8cb
miniduikboot <5243971+miniduikboot@users.noreply.github.com>
2021-02-27 13:08:04 +0100

    Also remove launcher containers after running

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
-exec docker run -it \
+
+exec docker run -it --rm \
     --mount type=bind,src="$(pwd)",dst=/work \
     --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
     expel-launcher \

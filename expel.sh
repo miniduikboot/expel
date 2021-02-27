e8585cd726e0fef095b84cb051532c95eca5e888
miniduikboot <5243971+miniduikboot@users.noreply.github.com>
2021-02-27 13:09:07 +0100

    Rename launcher.sh to expel.sh
    This makes more sense:
    - expel.py runs using system Python
    - expel.sh runs the expel.py in the container

--- expel.sh
+++ expel.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Start EXPEL inside a docker container and forward arguments to there
+
+# /!\ Note for contributors: If you make changes here, keep expel.cmd in sync please.
+
 exec docker run -it --rm \
     --mount type=bind,src="$(pwd)",dst=/work \
     --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \

1008a59abf4ff4910b5720f14ef05af45dd118f7
miniduikboot <5243971+miniduikboot@users.noreply.github.com>
2021-02-27 13:39:10 +0100

    Simplify Linux build script
    Was too lazy to implement a for loop in CMD

--- build.sh
+++ build.sh
@@ -5,10 +5,8 @@ set -euo pipefail
 
 export DOCKER_BUILDKIT=1
 # As plugin-build is just copying files and server-run actually installs things, create server-run first to save time.
-for target in server-run plugin-build
-do
-    docker build -t expel-$target --target $target . 
-done
+docker build -t expel-server-run --target server-run .
+docker build -t expel-plugin-build --target plugin-build .
 
 # The launcher is in a seperate file because it is completely independent of the other stages
 docker build -t expel-launcher -f Dockerfile.launcher .
